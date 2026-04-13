50c7ebf2f498f545bdc5c445ec29c6f9dcbbb5ef
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-13 13:19:32 -0300

    Agrego dependenia de Banco al administrador y los métodos para crear y eliminar cuentas

--- src/main/java/proyectoBanco/Banco.java
+++ src/main/java/proyectoBanco/Banco.java
@@ -4,13 +4,25 @@ import java.util.HashMap;
 
 public class Banco {
     private HashMap<String, Cuenta> cuentas;
+    private Administrador administrador;
 
     public Banco() {
         this.cuentas = new HashMap<>();
     }
 
+    public void cambiarAdministrador(Administrador administrador) {
+        this.administrador = administrador;
+    }
+
     public Cuenta obtenerCuenta(String nombrePropietario) {
         return this.cuentas.get(nombrePropietario);
+    }
+
+    public void solicitarCrearCuenta(TipoCuenta tipoCuenta, String nombrePropietario, String direccion) {
+        administrador.solicitarCrearCuenta(tipoCuenta, nombrePropietario, direccion);
+    }
+    public void solicitarEliminarCuenta(String nombrePropietario) {
+        administrador.solicitarEliminarCuenta(nombrePropietario);
     }
 
     public void crearCuenta(TipoCuenta tipoCuenta, String nombrePropietario, String direccion) {
--- src/main/java/proyectoBanco/ServicioBanco.java
+++ src/main/java/proyectoBanco/ServicioBanco.java
@@ -10,4 +10,10 @@ public class ServicioBanco {
     public Cuenta obtenerCuenta(String nombrePropietario) {
         return this.banco.obtenerCuenta(nombrePropietario);
     }
+    public void solicitarCrearCuenta(TipoCuenta tipoCuenta, String nombre, String direccion) {
+        this.banco.solicitarCrearCuenta(tipoCuenta, nombre, direccion);
+    }
+    public void solicitarEliminarCuenta(String nombre) {
+        this.banco.solicitarEliminarCuenta(nombre);
+    }
 }

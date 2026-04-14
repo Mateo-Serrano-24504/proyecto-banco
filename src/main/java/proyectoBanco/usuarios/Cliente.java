1bc60679eca5c03751ef9858c1e3f7e5944c965d
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-14 12:52:44 -0300

    Agrego la clase Cliente

---
+++ src/main/java/proyectoBanco/usuarios/Cliente.java
@@ -0,0 +1,32 @@
+package proyectoBanco.usuarios;
+
+import proyectoBanco.banco.ServicioBanco;
+import proyectoBanco.cuentas.Cuenta;
+
+public class Cliente extends Usuario {
+    private Cuenta vistaCuenta;
+
+    public Cliente(ServicioBanco servicioBanco, CredencialesUsuario credencialesUsuario) {
+        super(servicioBanco, credencialesUsuario);
+        this.vistaCuenta = null;
+    }
+
+    public void actualizarVistaCuenta() {
+        this.vistaCuenta = this.servicioBanco.obtenerEstadoCuenta(super.credencialesUsuario);
+    }
+    public boolean depositar(int cantidad) {
+        return this.servicioBanco.depositar(cantidad, super.credencialesUsuario);
+    }
+    public boolean retirar(int cantidad) {
+        return this.servicioBanco.retirar(cantidad, super.credencialesUsuario);
+    }
+    public boolean transferir(String receptor, int cantidad) {
+        return this.servicioBanco.transferir(receptor, cantidad, super.credencialesUsuario);
+    }
+    public void verEstadoCuenta() {
+        if (this.vistaCuenta == null) {
+            return;
+        }
+        this.vistaCuenta.verBalance();
+    }
+}

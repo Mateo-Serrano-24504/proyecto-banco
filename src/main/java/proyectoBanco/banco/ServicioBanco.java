6c030e2736fb9d28119c1a8800d85a30fc97bed6
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-14 14:01:56 -0300

    Agrego verificaciones de credenciales a los métodos de ServicioBanco

--- src/main/java/proyectoBanco/banco/ServicioBanco.java
+++ src/main/java/proyectoBanco/banco/ServicioBanco.java
@@ -6,28 +6,42 @@ import proyectoBanco.usuarios.CredencialesUsuario;
 
 public class ServicioBanco {
     private final Sucursal sucursal;
+    private final GestorUsuarios gestorUsuarios;
 
-    public ServicioBanco(Sucursal sucursal) {
+    private boolean verificar(CredencialesUsuario credencialesUsuario) {
+        return this.gestorUsuarios.verificarCredencialesUsuario(credencialesUsuario);
+    }
+
+    public ServicioBanco(Sucursal sucursal, GestorUsuarios gestorUsuarios) {
         this.sucursal = sucursal;
+        this.gestorUsuarios = gestorUsuarios;
     }
 
     // Operaciones de cliente
-    public void solicitarCrearCuenta(TipoCuenta tipoCuenta, CredencialesUsuario credencialesUsuario) {
+    public boolean solicitarCrearCuenta(TipoCuenta tipoCuenta, CredencialesUsuario credencialesUsuario) {
+        if (!this.verificar(credencialesUsuario)) return false;
         this.sucursal.solicitarCrearCuenta(tipoCuenta, credencialesUsuario);
+        return true;
     }
-    public void solicitarEliminarCuenta(CredencialesUsuario credencialesUsuario) {
+    public boolean solicitarEliminarCuenta(CredencialesUsuario credencialesUsuario) {
+        if (!this.verificar(credencialesUsuario)) return false;
         this.sucursal.solicitarEliminarCuenta(credencialesUsuario);
+        return true;
     }
     public Cuenta obtenerEstadoCuenta(CredencialesUsuario credencialesUsuario) {
+        if (!this.verificar(credencialesUsuario)) return null;
         return this.sucursal.obtenerEstadoCuenta(credencialesUsuario);
     }
     public boolean depositar(int cantidad, CredencialesUsuario credencialesUsuario) {
+        if (!this.verificar(credencialesUsuario)) return false;
         return this.sucursal.intentarHacerDeposito(cantidad, credencialesUsuario);
     }
     public boolean retirar(int cantidad, CredencialesUsuario credencialesUsuario) {
+        if (!this.verificar(credencialesUsuario)) return false;
         return this.sucursal.intentarHacerRetiro(cantidad, credencialesUsuario);
     }
     public boolean transferir(String receptor, int cantidad, CredencialesUsuario credencialesUsuario) {
+        if (!this.verificar(credencialesUsuario)) return false;
         return this.sucursal.intentarHacerTransferencia(
                 receptor,
                 cantidad,

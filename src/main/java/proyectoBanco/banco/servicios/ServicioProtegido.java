1d27cae611ca0f62c3a47e0a00670e692f0df078
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-15 12:57:46 -0300

    Creo una clase ServicioProtegido para usarla de clas base en los decoradores de servicio con funcionalidad de proteccion

---
+++ src/main/java/proyectoBanco/banco/servicios/ServicioProtegido.java
@@ -0,0 +1,16 @@
+package proyectoBanco.banco.servicios;
+
+import proyectoBanco.banco.GestorUsuarios;
+import proyectoBanco.usuarios.CredencialesUsuario;
+
+public class ServicioProtegido {
+    protected GestorUsuarios gestorUsuarios;
+
+    protected ServicioProtegido(GestorUsuarios gestorUsuarios) {
+        this.gestorUsuarios = gestorUsuarios;
+    }
+
+    protected boolean validarCredenciales(CredencialesUsuario credencialesUsuario) {
+        return this.gestorUsuarios.verificarCredencialesUsuario(credencialesUsuario);
+    }
+}

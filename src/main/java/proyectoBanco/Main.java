211ee7e3ca5775edd8e6efc53098de3f7b973f00
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-15 17:13:09 -0300

    Limpio Main

--- src/main/java/proyectoBanco/Main.java
+++ src/main/java/proyectoBanco/Main.java
@@ -1,17 +1,5 @@
 package proyectoBanco;
 
-import proyectoBanco.banco.*;
-import proyectoBanco.cuentas.CreadorCuenta;
-import proyectoBanco.cuentas.Cuenta;
-import proyectoBanco.cuentas.TipoCuenta;
-import proyectoBanco.usuarios.Cliente;
-import proyectoBanco.usuarios.PerfilUsuario;
-import proyectoBanco.usuarios.RolUsuario;
-import proyectoBanco.usuarios.UsuarioGestorCuentas;
-
-import java.util.HashMap;
-import java.util.HashSet;
-
 class Main {
     public static void main(String[] args) {}
 }

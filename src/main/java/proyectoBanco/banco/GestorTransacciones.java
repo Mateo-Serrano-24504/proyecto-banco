0dd97837781938633bbe4ec756a092d388edc5e8
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-15 17:08:13 -0300

    Modifico el GestorTransacciones para que use la base de datos

--- src/main/java/proyectoBanco/banco/GestorTransacciones.java
+++ src/main/java/proyectoBanco/banco/GestorTransacciones.java
@@ -3,9 +3,35 @@ package proyectoBanco.banco;
 import proyectoBanco.cuentas.Cuenta;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class GestorTransacciones {
     private final HashMap<String, Cuenta> cuentas;
+
+    private boolean manejarDepositoEnBdd(Map<String, Cuenta> cuentas, String usuario, int saldo) {
+        var cuenta = cuentas.get(usuario);
+        if (cuenta == null) {
+            return false;
+        }
+        cuenta.depositar(saldo);
+        return true;
+    }
+    private boolean manejarRetiroEnBdd(Map<String, Cuenta> cuentas, String usuario, int saldo) {
+        var cuenta = cuentas.get(usuario);
+        if (cuenta == null) {
+            return false;
+        }
+        cuenta.retirar(saldo);
+        return true;
+    }
+    private boolean manejarTransferenciaEnBdd(Map<String, Cuenta> cuentas, String usuario, String receptor, int saldo) {
+        var cuentaEmisor = cuentas.get(usuario);
+        var cuentaReceptor = cuentas.get(receptor);
+        if (cuentaEmisor == null || cuentaReceptor == null) {
+            return false;
+        }
+        return cuentaEmisor.transferir(cuentaReceptor, saldo);
+    }
 
     public GestorTransacciones(
             HashMap<String, Cuenta> cuentas
@@ -14,27 +40,24 @@ public class GestorTransacciones {
     }
 
     public boolean manejarDeposito(String usuario, int saldo) {
-        var cuenta = this.cuentas.get(usuario);
-        if (cuenta == null) {
-            return false;
-        }
-        cuenta.depositar(saldo);
-        return true;
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.manejarDepositoEnBdd(
+                        bdd.cuentas, usuario, saldo
+                )
+        );
     }
     public boolean manejarRetiro(String usuario, int saldo) {
-        var cuenta = this.cuentas.get(usuario);
-        if (cuenta == null) {
-            return false;
-        }
-        cuenta.retirar(saldo);
-        return true;
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.manejarRetiroEnBdd(
+                        bdd.cuentas, usuario, saldo
+                )
+        );
     }
     public boolean manejarTransferencia(String usuario, String receptor, int saldo) {
-        var cuentaEmisor = this.cuentas.get(usuario);
-        var cuentaReceptor = this.cuentas.get(receptor);
-        if (cuentaEmisor == null || cuentaReceptor == null) {
-            return false;
-        }
-        return cuentaEmisor.transferir(cuentaReceptor, saldo);
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.manejarTransferenciaEnBdd(
+                        bdd.cuentas, usuario, receptor, saldo
+                )
+        );
     }
 }

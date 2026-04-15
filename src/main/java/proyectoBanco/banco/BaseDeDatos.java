21c1d42ac338ab31f58abeec5c2137ce93279bec
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-15 17:03:24 -0300

    Agrego los roles en la base de datos y modifico el GestorRoles para que empiece a usarla

--- src/main/java/proyectoBanco/banco/BaseDeDatos.java
+++ src/main/java/proyectoBanco/banco/BaseDeDatos.java
@@ -2,16 +2,20 @@ package proyectoBanco.banco;
 
 import proyectoBanco.cuentas.Cuenta;
 import proyectoBanco.usuarios.PerfilUsuario;
+import proyectoBanco.usuarios.RolUsuario;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class BaseDeDatos {
     public final Map<String, Cuenta> cuentas;
     public final Map<String, PerfilUsuario> periles;
+    public final Map<String, Set<RolUsuario>> roles;
 
     BaseDeDatos() {
         this.cuentas = new HashMap<>();
         this.periles = new HashMap<>();
+        this.roles = new HashMap<>();
     }
 }
--- src/main/java/proyectoBanco/banco/GestorRoles.java
+++ src/main/java/proyectoBanco/banco/GestorRoles.java
@@ -3,40 +3,72 @@ package proyectoBanco.banco;
 import proyectoBanco.usuarios.RolUsuario;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class GestorRoles {
     private final HashMap<String, Set<RolUsuario>> rolesUsuario;
+
+    private void agregarRolesDeUsuarioEnBdd(Map<String, Set<RolUsuario>> roles, String nombre, Set<RolUsuario> rolesUsuario) {
+        if (!roles.containsKey(nombre)) {
+            roles.put(nombre, rolesUsuario);
+        } else {
+            roles.get(nombre).addAll(rolesUsuario);
+        }
+    }
+    private void eliminarRolDeUsuarioEnBdd(Map<String, Set<RolUsuario>> roles, String nombre, RolUsuario rolUsuario) {
+        var rolesActuales = roles.get(nombre);
+        if (rolesActuales == null || rolesActuales.isEmpty()) {
+            return;
+        }
+        rolesActuales.remove(rolUsuario);
+        if (rolesActuales.isEmpty()) {
+            roles.remove(nombre);
+        }
+    }
+    private boolean verificarUsuarioTieneRolesEnBdd(Map<String, Set<RolUsuario>> roles, String nombre) {
+        return roles.containsKey(nombre);
+    }
+    public boolean verificarRolEnUsuarioEnBdd(Map<String, Set<RolUsuario>> roles, String usuario, RolUsuario rolUsuario) {
+        var rolesActuales = roles.get(usuario);
+        if (rolesActuales == null || rolesActuales.isEmpty()) {
+            return false;
+        }
+        return rolesActuales.contains(rolUsuario);
+    }
 
     public GestorRoles() {
         this.rolesUsuario = new HashMap<>();
     }
 
     public void agregarRolesDeUsuario(String nombre, Set<RolUsuario> rolesUsuario) {
-        if (!this.rolesUsuario.containsKey(nombre)) {
-            this.rolesUsuario.put(nombre, rolesUsuario);
-        } else {
-            this.rolesUsuario.get(nombre).addAll(rolesUsuario);
-        }
+        AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> {
+                    this.agregarRolesDeUsuarioEnBdd(
+                            bdd.roles, nombre, rolesUsuario
+                    );
+                    return null;
+                }
+        );
     }
     public void eliminarRolDeUsuario(String nombre, RolUsuario rolUsuario) {
-        var roles = this.rolesUsuario.get(nombre);
-        if (roles == null || roles.isEmpty()) {
-            return;
-        }
-        roles.remove(rolUsuario);
-        if (roles.isEmpty()) {
-            this.rolesUsuario.remove(nombre);
-        }
+        AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> {
+                    this.eliminarRolDeUsuarioEnBdd(
+                            bdd.roles, nombre, rolUsuario
+                    );
+                    return null;
+                }
+        );
     }
     public boolean usuarioTieneRoles(String usuario) {
-        return this.rolesUsuario.containsKey(usuario);
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.verificarUsuarioTieneRolesEnBdd(bdd.roles, usuario)
+        );
     }
-    public boolean verificarUsuarioConCredenciales(String usuario, RolUsuario rolUsuario) {
-        var roles = this.rolesUsuario.get(usuario);
-        if (roles == null || roles.isEmpty()) {
-            return false;
-        }
-        return roles.contains(rolUsuario);
+    public boolean verificarRolesEnUsuario(String usuario, RolUsuario rolUsuario) {
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.verificarRolEnUsuarioEnBdd(bdd.roles, usuario, rolUsuario)
+        );
     }
 }

4eb12f3794a8acbc67dcbb3dc38a3bb8dc2962c5
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-15 17:23:16 -0300

    Cambio GestorRoles para que no interactúe con la BDD directamente,en us lugar GestorUsuarios le pasa el mapa del que ya tomó lock, y ahora GestorUsuarios usa BaseDeDatos

--- src/main/java/proyectoBanco/banco/GestorRoles.java
+++ src/main/java/proyectoBanco/banco/GestorRoles.java
@@ -6,14 +6,16 @@ import java.util.Map;
 import java.util.Set;
 
 public class GestorRoles {
-    private void agregarRolesDeUsuarioEnBdd(Map<String, Set<RolUsuario>> roles, String nombre, Set<RolUsuario> rolesUsuario) {
+    public GestorRoles() {}
+
+    public void agregarRolesDeUsuario(Map<String, Set<RolUsuario>> roles, String nombre, Set<RolUsuario> rolesUsuario) {
         if (!roles.containsKey(nombre)) {
             roles.put(nombre, rolesUsuario);
         } else {
             roles.get(nombre).addAll(rolesUsuario);
         }
     }
-    private void eliminarRolDeUsuarioEnBdd(Map<String, Set<RolUsuario>> roles, String nombre, RolUsuario rolUsuario) {
+    public void eliminarRolDeUsuario(Map<String, Set<RolUsuario>> roles, String nombre, RolUsuario rolUsuario) {
         var rolesActuales = roles.get(nombre);
         if (rolesActuales == null || rolesActuales.isEmpty()) {
             return;
@@ -23,35 +25,7 @@ public class GestorRoles {
             roles.remove(nombre);
         }
     }
-    private boolean verificarUsuarioTieneRolesEnBdd(Map<String, Set<RolUsuario>> roles, String nombre) {
+    public boolean verificarUsuarioTieneRoles(Map<String, Set<RolUsuario>> roles, String nombre) {
         return roles.containsKey(nombre);
-    }
-
-    public GestorRoles() {}
-
-    public void agregarRolesDeUsuario(String nombre, Set<RolUsuario> rolesUsuario) {
-        AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
-                bdd -> {
-                    this.agregarRolesDeUsuarioEnBdd(
-                            bdd.roles, nombre, rolesUsuario
-                    );
-                    return null;
-                }
-        );
-    }
-    public void eliminarRolDeUsuario(String nombre, RolUsuario rolUsuario) {
-        AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
-                bdd -> {
-                    this.eliminarRolDeUsuarioEnBdd(
-                            bdd.roles, nombre, rolUsuario
-                    );
-                    return null;
-                }
-        );
-    }
-    public boolean usuarioTieneRoles(String usuario) {
-        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
-                bdd -> this.verificarUsuarioTieneRolesEnBdd(bdd.roles, usuario)
-        );
     }
 }
--- src/main/java/proyectoBanco/banco/GestorUsuarios.java
+++ src/main/java/proyectoBanco/banco/GestorUsuarios.java
@@ -4,43 +4,56 @@ import proyectoBanco.usuarios.CredencialesUsuario;
 import proyectoBanco.usuarios.PerfilUsuario;
 import proyectoBanco.usuarios.RolUsuario;
 
-import java.util.HashMap;
 import java.util.Set;
 
 public class GestorUsuarios {
     private final GestorRoles gestorRoles;
-    private final HashMap<String, PerfilUsuario> perfilesUsuarios;
 
-    public GestorUsuarios(GestorRoles gestorRoles) {
-        this.gestorRoles = gestorRoles;
-        this.perfilesUsuarios = new HashMap<>();
-    }
-
-    public boolean verificarCredencialesUsuario(CredencialesUsuario credencialesUsuario) {
-        var perfilUsuario = this.perfilesUsuarios.get(credencialesUsuario.usuario());
+    private boolean verificarCredencialesUsuarioEnBdd(BaseDeDatos bdd, CredencialesUsuario credencialesUsuario) {
+        var perfilUsuario = bdd.perfiles.get(credencialesUsuario.usuario());
         if (perfilUsuario == null) {
             return false;
         }
         return perfilUsuario.obtenerContr().equals(credencialesUsuario.contr());
     }
-    public boolean agregarUsuarioSiNoExiste(PerfilUsuario perfilUsuario, Set<RolUsuario> rolesUsuario) {
-        var perfil = this.perfilesUsuarios.get(perfilUsuario.obtenerNombre());
+    private boolean agregarUsuarioSiNoExisteEnBdd(BaseDeDatos bdd, PerfilUsuario perfilUsuario, Set<RolUsuario> rolesUsuario) {
+        var perfil = bdd.perfiles.get(perfilUsuario.obtenerNombre());
         if (perfil != null) {
             return perfil.generarCredenciales().equals(perfilUsuario.generarCredenciales());
         }
-        this.perfilesUsuarios.put(perfilUsuario.obtenerNombre(), perfilUsuario);
-        this.gestorRoles.agregarRolesDeUsuario(perfilUsuario.obtenerNombre(), rolesUsuario);
+        bdd.perfiles.put(perfilUsuario.obtenerNombre(), perfilUsuario);
+        this.gestorRoles.agregarRolesDeUsuario(bdd.roles, perfilUsuario.obtenerNombre(), rolesUsuario);
         return true;
     }
-    public boolean eliminarRolDeUsuarioSiExiste(CredencialesUsuario credencialesUsuario, RolUsuario rolUsuario) {
+    private boolean eliminarRolDeUsuarioSiExisteEnBdd(BaseDeDatos bdd, CredencialesUsuario credencialesUsuario, RolUsuario rolUsuario) {
         var nombre = credencialesUsuario.usuario();
-        if (this.perfilesUsuarios.containsKey(nombre)) {
+        if (bdd.perfiles.containsKey(nombre)) {
             return false;
         }
-        this.gestorRoles.eliminarRolDeUsuario(nombre, rolUsuario);
-        if (!this.gestorRoles.usuarioTieneRoles(nombre)) {
-            this.perfilesUsuarios.remove(nombre);
+        this.gestorRoles.eliminarRolDeUsuario(bdd.roles, nombre, rolUsuario);
+        if (!this.gestorRoles.verificarUsuarioTieneRoles(bdd.roles, nombre)) {
+            bdd.perfiles.remove(nombre);
         }
         return true;
+    }
+
+    public GestorUsuarios(GestorRoles gestorRoles) {
+        this.gestorRoles = gestorRoles;
+    }
+
+    public boolean verificarCredencialesUsuario(CredencialesUsuario credencialesUsuario) {
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.verificarCredencialesUsuarioEnBdd(bdd, credencialesUsuario)
+        );
+    }
+    public boolean agregarUsuarioSiNoExiste(PerfilUsuario perfilUsuario, Set<RolUsuario> rolesUsuario) {
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.agregarUsuarioSiNoExisteEnBdd(bdd, perfilUsuario, rolesUsuario)
+        );
+    }
+    public boolean eliminarRolDeUsuarioSiExiste(CredencialesUsuario credencialesUsuario, RolUsuario rolUsuario) {
+        return AccesoBaseDeDatos.ejecutarSobreBaseDeDatos(
+                bdd -> this.eliminarRolDeUsuarioSiExisteEnBdd(bdd, credencialesUsuario, rolUsuario)
+        );
     }
 }

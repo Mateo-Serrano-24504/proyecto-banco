635be8dd3a519707fb64d45c044bca9f57e5819a
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2026-04-15 16:51:59 -0300

    Corrijo el nombre del método de AccesoBaseDeDatos

--- src/main/java/proyectoBanco/banco/AccesoBaseDeDatos.java
+++ src/main/java/proyectoBanco/banco/AccesoBaseDeDatos.java
@@ -7,7 +7,7 @@ public class AccesoBaseDeDatos {
     private static final Object lock = new Object();
     private static final BaseDeDatos baseDeDatos = new BaseDeDatos();
 
-    private static <T> T ejecutarSobreCuentas(Function<BaseDeDatos, T> funcion) {
+    public static <T> T ejecutarSobreBaseDeDatos(Function<BaseDeDatos, T> funcion) {
         synchronized (AccesoBaseDeDatos.lock) {
             return funcion.apply(AccesoBaseDeDatos.baseDeDatos);
         }
